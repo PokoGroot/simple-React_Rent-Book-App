711949f9150fa927ea60a210894d72db40139f73
Poko Groot <PokoGroot@users.noreply.github.com>
2019-08-27 13:01:03 +0700

    test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom'
-import { Provider } from 'react-redux'
 
 import Home from './pages/home/home'
 import Login from './pages/login/login'
@@ -16,16 +15,14 @@ import './App.css';
 const App = () => {
   return (
     <Router>
-      <Provider store={store}>
-        <Switch>
-            <Route exact path={'/'} component={Home} /> 
-            <Route path={'/login'} component={Login} />
-            <Route path={'/register'} component={Register} />
-            <Route path={'/detail_book/:id'} component={DetailBook} />
-            <Route path={'/explore'} component={Explore} />
-            <Redirect to="/" />
-        </Switch>
-      </Provider>
+      <Switch>
+          <Route exact path={'/'} component={Home} /> 
+          <Route path={'/login'} component={Login} />
+          <Route path={'/register'} component={Register} />
+          <Route path={'/detail_book/:id'} component={DetailBook} />
+          <Route path={'/explore'} component={Explore} />
+          <Redirect to="/" />
+      </Switch>
     </Router>
   );
 }
--- src/Publics/Reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux'
-
-import itemList from './item'
-
-const rootReducer = combineReducers ({
-    itemList,
-})
-
-export default rootReducer
