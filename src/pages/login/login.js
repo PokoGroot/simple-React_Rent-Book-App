8166a10f9de4e862cd3f405de0421d964d2e6c0f
Poko Groot <PokoGroot@users.noreply.github.com>
2019-09-02 00:00:04 +0700

    token

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -41,7 +41,7 @@ class Login extends React.Component {
                         modalLoginFalse: true
                     })
                 }else{
-                    window.localStorage.setItem("token", res.action.payload.data.token)
+                    localStorage.setItem("token", res.action.payload.data.token)
                     this.loggingIn()
                 }
             })
